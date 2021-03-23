5000cc80cddd28f6365e83644a3b9577c355ab2f
cleandevcode <freelance1331@gmail.com>
2021-03-23 20:55:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+
+import "./App.css";
+
+const sortIcon = <ArrowDownward />;
+const columns = [
+  {
+    name: "Avatar",
+    selector: "avatar_url",
+    cell: row => <img alt="" src={row.avatar_url} />
+  },
+  {
+    name: "Login",
+    selector: "login",
+    sortable: true
+  },
+  {
+    name: "Type",
+    selector: "type"
+  }
+];
 
 function App() {
+  const [login, setLogin] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleChange = e => {
+    setLogin(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    const url = `https://api.github.com/search/users?q=${login}%20in:login`;
+    fetch(url)
+      .then(result => result.json())
+      .then(res => {
+        setData(res);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <input
+          value={login}
+          placeholder="Enter the login..."
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <DataTable
+        title=""
+        data={data && data.items}
+        columns={columns}
+        selectableRows
+        sortIcon={sortIcon}
+        progressPending={loading}
+        pagination
+      />
     </div>
   );
 }
